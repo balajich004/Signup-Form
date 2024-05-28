ab229fcd15a903f9cdd187ea439f3623c5b0ebf4
balajich004 <balajichennupati123@gmail.com>
2024-05-28 20:39:40 +0530

    completed all the validation yet to add some more warnings

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,30 @@
 
-const obj={};
+const inputs = document.querySelectorAll("input"); 
+
+const obj = {
+    firstname: /^\w{5,10}$/i,
+    lastname: /^\w{5,10}$/i,
+    email: /^([a-z\d\.-]+)@([a-z\d\.-]+)\.([a-z]{2,8})$/i,
+    phone: /^\d{10}$/i,
+    password: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/i,
+};
+
+const validate = (field, exp) => {
+    if (exp.test(field.value)) {
+        field.classList.add("valid");
+        field.classList.remove("invalid");
+    } else {
+        field.classList.add("invalid");
+        field.classList.remove("valid");
+    }
+};
+
+Array.from(inputs).forEach(input => {
+    input.addEventListener("keyup", (e) => {
+        const validationPattern = obj[e.target.name];
+        if (validationPattern) {
+            validate(e.target, validationPattern);
+        }
+        console.log(e.target.name);
+    });
+});
